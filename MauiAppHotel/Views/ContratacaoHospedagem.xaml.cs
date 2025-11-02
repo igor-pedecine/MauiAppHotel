689e05d6fec2c9cd63f7f282db9545685af770bc
Samsung <Samsung@DESKTOP-3IOUFVL>
2025-11-02 13:04:35 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiAppHotel.Views
+{
+    public partial class ContratacaoHospedagem : ContentPage
+    {
+        public ContratacaoHospedagem()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSobreClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Sobre());
+        }
+    }
+}
---
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -0,0 +1,15 @@
+namespace MauiAppHotel.Views
+{
+    public partial class Sobre : ContentPage
+    {
+        public Sobre()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnVoltarClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
